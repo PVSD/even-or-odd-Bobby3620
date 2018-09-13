53c044d86a2bf01a6db62fd125e05960ad79f16e
rd568 <bobbyday3620@gmail.com>
2018-09-13 11:19:06 -0400

    ayyyyyyy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner kbInput = new Scanner(System.in);
+
+        //Enter an int
+        System.out.println("Enter an integer: ");
+        int kbInt = kbInput.nextInt();
+        int kbEven = kbInt % 2;
+
+        if(kbEven == 0){
+            System.out.println("The integer " + kbInt + " is even.");
+        }else {
+            System.out.println("The integer " + kbInt + " is even.");
+        }
+
+
+        //Decides if odd or even
+
+
     }
 }
